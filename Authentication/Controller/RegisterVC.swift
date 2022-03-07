9a623a73e01c8f2c97241004883a01e38f66d5c9
Hamza Shahbaz <60182259+muneebzain@users.noreply.github.com>
2022-03-07 16:38:22 +0500

    Initial Commit

---
+++ Authentication/Controller/RegisterVC.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterVC.swift
+//  Authentication
+//
+//  Created by Muneeb Zain on 07/03/2022.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+  
+
+}
