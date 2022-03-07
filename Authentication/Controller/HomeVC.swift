1bebe3ac5ffb0585f0624bb0692d8e15a6443fa8
Hamza Shahbaz <60182259+muneebzain@users.noreply.github.com>
2022-03-07 16:43:37 +0500

    Design ready

---
+++ Authentication/Controller/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  HomeVC.swift
+//  Authentication
+//
+//  Created by Muneeb Zain on 07/03/2022.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
